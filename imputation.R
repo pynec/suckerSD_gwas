9a85ec0937f5c43cd5028f71ad06458a3d2701bb
Cassandre Pyne <cassandrepyne@Cassandres-MacBook-Air.local>
2020-06-03 14:33:55 -0400

    updated Graham script - runs successfully

--- imputation.R
+++ imputation.R
@@ -1,17 +1,16 @@
-setwd("/Users/cassandrepyne/Documents")
-
-##script that attempts to get genotype likelihoods from hdf5 files 
+#imputation script for running on a high performance computing cluster 
 library(rhdf5)
 library(stringi)
 
-#h5ls("mm25maf05_k3_100k_rep1.hdf5")
-##load in genotype likelihood files 
-gprob1 = h5read("mm25maf05_k3_100k_rep1.hdf5", "gprob")  
-gprob2 = h5read("mm25maf05_k3_100k_rep2.hdf5", "gprob")  
-gprob3 = h5read("mm25maf05_k3_100k_rep3.hdf5", "gprob")  
+args <- commandArgs(trailingOnly = TRUE)
 
-##following from hdf52rdata.R from buerklelab bitbucket
+gprob1 = h5read(args[1], "gprob")
+gprob2 = h5read(args[2], "gprob")
+gprob3 = h5read(args[3], "gprob")
+
+##following adjusted from hdf52rdata.R from beurklelab bitbucket
 ploidy <- dim(gprob1)[1] - 1
+
 gp_mean1 <- apply(gprob1, c(2,3), function(x){sum(x*(0:ploidy))})
 gp_mean2 <- apply(gprob2, c(2,3), function(x){sum(x*(0:ploidy))})
 gp_mean3 <- apply(gprob3, c(2,3), function(x){sum(x*(0:ploidy))})
@@ -21,235 +20,61 @@ gp.mean <- (gp_mean1+gp_mean2+gp_mean3)/3
 nind = 89
 nloci = 68186
 
----------------------------------------------------------------------------
-##Testing with loci = 1000
+##testing with all loci (68186)
 
-##data frame to add genotype likelihood to (GL for each loci & ind)
-test_df <- data.frame(matrix(ncol = nind, nrow = 1000))
-for(i in 1:1000){
-	test_df[i, ] = gp.mean[, i]
-}
+##data frame to add genotype likelihoods to (GL for each loci and ind)
+
+test_df = data.frame(matrix(ncol = nind, nrow = nloci))
+
+for(i in 1:nloci){
+      test_df[i, ] = gp.mean[, i]
+ }
+ 
 colnames(test_df) = NULL
 
-variants = read.csv("variants.txt", nrows = 1000, skip = 1, header = F)
+variants = read.csv(args[4], nrows = nloci, skip = 1, header = F)
+
 chr = variants$V1
-pos = variants$V2
+pos = variants$V2 
 
-##output to add to genotype file (with chromosome information and commas - for GEMMA)
-output <- data.frame(matrix(ncol = nind + 1, nrow = 1000))
-for(i in 1:1000){
-	output[i, 1] = paste0(chr[i], ", ", "x", ", ", "x")
-	index = 0 
-	for(j in 2:(nind+2)){
-		if(j == nind +2){
-			output[i,j] = paste0(test_df[i,index])
-		}
-		else{
-		output[i,j] = paste0(test_df[i,index], ", ")
-		index = index + 1	
-	}}	}
-	colnames(output) = NULL  
+##output to add to genotype file (with chr information and commas - for GEMMA)
+output =  data.frame(matrix(ncol = nind + 1, nrow = nloci))
 
-##genotype file 
-for(i in 1:1000){
-	write.table(output[i,], "geno_real_1000.txt", append = TRUE, row.names = FALSE, quote = FALSE)
+for(i in 1:nloci){
+      output[i, 1] = paste0(chr[i], ", ", "x", ", ", "x")
+      index = 0
+      for(j in 2:(nind+2)){
+      	    if(j == nind +2){
+	    output[i,j] = paste0(test_df[i,index])
+	    }
+	    else{
+	    output[i,j] = paste0(test_df[i,index], ", ")
+	    index = index + 1
+	    } } }
+colnames(output) = NULL
+
+##setting up genotype file
+for(i in 1:nloci){
+      write.table(output[i,], "geno_real_68186.txt", append = TRUE, row.names = FALSE, quote = FALSE)
 }
 
+
 ##output for annotation file
-anno <- data.frame(matrix(ncol = 1, nrow = 1000))
-for(i in 1:1000){
-	anno[i, ] = paste0(chr[i], sample(1:20, 1), " ", pos[i], " ", chr[i])
+anno = data.frame(matrix(ncol = 1, nrow = nloci))
+for(i in 1:nloci){
+anno[i,] = paste0(chr[i], sample(1:20,1), " ", pos[i], " ", chr[i])
 }
 colnames(anno) = NULL
 
-##annotation file 
-for(i in 1:1000){
-	write.table(anno[i,], "anno_real_1000.txt", append = TRUE, row.names= FALSE, quote = FALSE)
-}
+##setting up annotation file
+for(i in 1:nloci){
+      write.table(anno[i,], "anno_real_68186.txt", append = TRUE, row.names = FALSE, quote = FALSE)
+      }
 
-##getting sex information on the individuals for pheontype file 
-details <- read.csv("sucker_metadata_knownsex_89.csv", header=T)
-sex <- details$sex 
-sex <- gsub("Male", "0", sex)
-sex <- gsub("Female", "1", sex)
+##getting sex information on the individuals for phenotype file
+details = read.csv(args[5], header = T)
+sex = details$sex
+sex = gsub("Male", "0", sex)
+sex = gsub("Female", "1", sex)
 
-##add to phenotype file 
-##remember that it randomly adds an X to the beginning of the file 
-write.table(sex, "pheno_real_1000.txt", append = TRUE, row.names = FALSE, quote = FALSE)
-
-
-
----------------------------------------------------------------------
-##Testing with loci = 10000
-
-##data frame to add genotype likelihood to (GL for each loci & ind)
-test_df <- data.frame(matrix(ncol = nind, nrow = 10000))
-for(i in 1:10000){
-	test_df[i, ] = gp.mean[, i]
-}
-colnames(test_df) = NULL
-
-variants = read.csv("variants.txt", nrows = 10000, skip = 1, header = F)
-
-chr = variants$V1
-
-pos = variants$V2
-
-##output to add to genotype file (with chromosome information and commas - for GEMMA)
-output <- data.frame(matrix(ncol = nind + 1, nrow = 10000))
-for(i in 1:10000){
-	output[i, 1] = paste0(chr[i], ", ", "x", ", ", "x")
-	index = 0 
-	for(j in 2:(nind+2)){
-		if(j == nind +2){
-			output[i,j] = paste0(test_df[i,index])
-		}
-		else{
-		output[i,j] = paste0(test_df[i,index], ", ")
-		index = index + 1	
-	}}	}
-	colnames(output) = NULL  
-
-##genotype file 
-for(i in 1:10000){
-	write.table(output[i,], "geno_real_10000.txt", append = TRUE, row.names = FALSE, quote = FALSE)
-}
-
-##output for annotation file
-anno <- data.frame(matrix(ncol = 1, nrow = 10000))
-for(i in 1:10000){
-	anno[i, ] = paste0(chr[i], sample(1:20, 1), " ", pos[i], " ", chr[i])
-}
-colnames(anno) = NULL
-
-##annotation file 
-for(i in 1:10000){
-	write.table(anno[i,], "anno_real_10000.txt", append = TRUE, row.names= FALSE, quote = FALSE)
-}
-
-##getting sex information on the individuals for pheontype file 
-details <- read.csv("sucker_metadata_knownsex_89.csv", header=T)
-sex <- details$sex 
-sex <- gsub("Male", "0", sex)
-sex <- gsub("Female", "1", sex)
-
-##add to phenotype file 
-##remember that it randomly adds an X to the beginning of the file 
-write.table(sex, "pheno_real_10000.txt", append = TRUE, row.names = FALSE, quote = FALSE)
-
-
----------------------------------------------------------------------
-##Testing with loci = 20000
-
-##data frame to add genotype likelihood to (GL for each loci & ind)
-test_df <- data.frame(matrix(ncol = nind, nrow = 20000))
-for(i in 1:20000){
-	test_df[i, ] = gp.mean[, i]
-}
-colnames(test_df) = NULL
-
-variants = read.csv("variants.txt", nrows = 20000, skip = 1, header = F)
-
-chr = variants$V1
-
-pos = variants$V2
-
-##output to add to genotype file (with chromosome information and commas - for GEMMA)
-output <- data.frame(matrix(ncol = nind + 1, nrow = 20000))
-for(i in 1:20000){
-	output[i, 1] = paste0(chr[i], ", ", "x", ", ", "x")
-	index = 0 
-	for(j in 2:(nind+2)){
-		if(j == nind +2){
-			output[i,j] = paste0(test_df[i,index])
-		}
-		else{
-		output[i,j] = paste0(test_df[i,index], ", ")
-		index = index + 1	
-	}}	}
-	colnames(output) = NULL  
-
-##genotype file 
-for(i in 1:20000){
-	write.table(output[i,], "geno_real_20000.txt", append = TRUE, row.names = FALSE, quote = FALSE)
-}
-
-##output for annotation file
-anno <- data.frame(matrix(ncol = 1, nrow = 20000))
-for(i in 1:20000){
-	anno[i, ] = paste0(chr[i], sample(1:20, 1), " ", pos[i], " ", chr[i])
-}
-colnames(anno) = NULL
-
-##annotation file 
-for(i in 1:34000){
-	write.table(anno[i,], "anno_real_20000.txt", append = TRUE, row.names= FALSE, quote = FALSE)
-}
-
-details <- read.csv("sucker_metadata_knownsex_89.csv", header=T)
-sex <- details$sex 
-sex <- gsub("Male", "0", sex)
-sex <- gsub("Female", "1", sex)
-
-##getting sex information on the individuals for pheontype file 
-##remember that it randomly adds an X to the beginning of the file 
-write.table(sex, "pheno_real_34000.txt", append = TRUE, row.names = FALSE, quote = FALSE)
-
----------------------------------------------------------------------
-##Testing with full data set
-
-##data frame to add genotype likelihood to (GL for each loci & ind)
-
-test_df <- data.frame(matrix(ncol = nind, nrow = 68186))
-for(i in 1:68186){
-	test_df[i, ] = gp.mean[, i]
-}
-colnames(test_df) = NULL
-
-variants = read.csv("variants.txt", nrows = 20000, skip = 1, header = F)
-
-chr = variants$V1
-
-pos = variants$V2
-
-##output to add to genotype file (with chromosome information and commas - for GEMMA)
-output <- data.frame(matrix(ncol = nind + 1, nrow = 20000))
-for(i in 1:20000){
-	output[i, 1] = paste0(chr[i], ", ", "x", ", ", "x")
-	index = 0 
-	for(j in 2:(nind+2)){
-		if(j == nind +2){
-			output[i,j] = paste0(test_df[i,index])
-		}
-		else{
-		output[i,j] = paste0(test_df[i,index], ", ")
-		index = index + 1	
-	}}	}
-	colnames(output) = NULL  
-
-##genotype file 
-for(i in 1:20000){
-	write.table(output[i,], "geno_real_20000.txt", append = TRUE, row.names = FALSE, quote = FALSE)
-}
-
-##output for annotation file
-anno <- data.frame(matrix(ncol = 1, nrow = 20000))
-for(i in 1:20000){
-	anno[i, ] = paste0(chr[i], sample(1:20, 1), " ", pos[i], " ", chr[i])
-}
-colnames(anno) = NULL
-
-##annotation file 
-for(i in 1:34000){
-	write.table(anno[i,], "anno_real_20000.txt", append = TRUE, row.names= FALSE, quote = FALSE)
-}
-##getting sex information on the individuals for pheontype file 
-details <- read.csv("sucker_metadata_knownsex_89.csv", header=T)
-sex <- details$sex 
-sex <- gsub("Male", "0", sex)
-sex <- gsub("Female", "1", sex)
-
-##add to phenotype file 
-##remember that it randomly adds an X to the beginning of the file 
-write.table(sex, "pheno_real_34000.txt", append = TRUE, row.names = FALSE, quote = FALSE)
 
